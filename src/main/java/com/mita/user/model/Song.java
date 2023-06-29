51f2abff6138650d276f71665ab77e82fc72ee22
pratej <pratejyadav51@gmail.com>
2023-06-29 21:34:22 +0530

    fields added in Song class

--- src/main/java/com/mita/user/model/Song.java
+++ src/main/java/com/mita/user/model/Song.java
@@ -3,15 +3,23 @@ package com.mita.user.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.List;
 
 @Getter
 @Setter
 public class Song {
+
     private String id;
     private String title;
+    private String Composers;
+    private String Genre;
+    private String featuredArtists;
     private String description;
     private List<String> audio;
+    private String album;
     private List<String> image;
+    private String language;
+    private Date releaseDate;
 
 }
