8358142222cc1e2d8b623b108d9f5665953cdcf3
pratej <pratejyadav51@gmail.com>
2023-06-29 20:04:55 +0530

    fields added in Artist class

--- src/main/java/com/mita/user/model/Artist.java
+++ src/main/java/com/mita/user/model/Artist.java
@@ -1,16 +1,26 @@
 package com.mita.user.model;
 
+import com.mita.user.model.base.HumanName;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
-public class Artist {
+public class Artist extends User{
+
     private String id;
-    private String name;
+    private HumanName humanName;
     private String description;
     private List<String> image;
+    private List<String> songs;
+    private List<String> genres;
+    private int followersCount;
+    private Map<String, String> socialMediaLinks;
+    private List<String> albums;
+    private List<String> events;
+
 
 }
